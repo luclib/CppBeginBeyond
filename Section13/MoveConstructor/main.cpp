800adf187fb227ae10824c72c91439c75d07ccbf
Lucas Liberman <lucas.liberman@novanta.com>
2023-04-28 10:36:47 -0400

    Added section in Makefile appendix section dealing with automatic variables.

--- Section13/MoveConstructor/main.cpp
+++ Section13/MoveConstructor/main.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <move.h>
 
+class Move {
+private:
+    int *data;
+public:
+    int get_data_value(){
+        return *data;
+    }
+    void set_data_value(int d){
+        *data = d;
+    }
+    Move(int d){
+        data = new int;
+        *data = d;
+        std::cout << "Constructor for: " << data
+            << std::endl;
+    }
+    Move(const Move &source)
+        : data{source.data} {
+        std::cout << "Copy constructor - deep"
+            << std::endl;
+    }
+    // Move(Move &&source) noexcept
+    //     :data {source.data}{
+    //         source.data = nullptr;
+    //         std::cout << "Move constructor -moving resource: " << std::endl;
+    // }
+   ~Move(){  
+        if(data != nullptr){
+            std::cout << "Destructor freeing data for: " << *data << std::endl;
+        }
+        else {
+            std::cout << "Destructor freeing data for nullptr" << std::endl;
+        }       
+    };
+};
 
-int main () {
+int main () { 
     std::vector<Move> vec;
-    
-    vec.push_back(Move{10});
+    vec.push_back(Move{10}); // No variable name creates temp object (i.e. an r-value)
     vec.push_back(Move{20});
+    // vec.push_back(Move{30});
+    // vec.push_back(Move{40});
+    // vec.push_back(Move{50});
+    // vec.push_back(Move{60});
+    // vec.push_back(Move{70});
+    // vec.push_back(Move{80});
     return 0;
 }
--- Section13/MoveConstructor/move.cpp
+++ Section13/MoveConstructor/move.cpp
@@ -2,7 +2,7 @@
 #include "move.h"
 
 int Move::get_data_value(){
-    return 0;
+    return *data;
 }
 void Move::set_data_value(int d){
     *data = d;
@@ -10,14 +10,25 @@ void Move::set_data_value(int d){
 Move::Move(int d){
     data = new int;
     *data = d;
+    std::cout << "Constructor for: " << data
+        << std::endl;
 }
 Move::Move(const Move &source)
     : data{source.data} {
     std::cout << "Copy constructor - deep"
         << std::endl;
 }
+Move::Move(Move &&source) noexcept
+    :data {source.data}{
+        source.data = nullptr;
+        std::cout << "Move constructor -moving resource: " << std::endl;
+}
 Move::~Move(){
+    if(data != nullptr) {
+        std::cout << "Destructor freeing data for: " << std::endl; 
+    }
+    else {
+        std::cout << "Destructor freeing data for nullptr" << std::endl;
+    }
     delete data;
-    std::cout << "Destructor freeing data"
-        << std::endl;
 }
